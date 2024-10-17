4a5762821bb0bf0257bb4567c7fff2fbfece754c
draw-n <helen.wu1219@gmail.com>
2024-10-16 22:16:23 -0500

    help

--- client/src/pages/profile/ProfileDropdown.tsx
+++ client/src/pages/profile/ProfileDropdown.tsx
@@ -1,4 +1,4 @@
-import { Space, Dropdown, Tooltip, Button, MenuProps, Flex } from "antd";
+import { Dropdown, Tooltip, Button, MenuProps, Flex } from "antd";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/AuthContext";
