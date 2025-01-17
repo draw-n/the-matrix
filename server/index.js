7f8ad2fb796c3855174dc749b526a42199012f4c
draw-n <helen.wu1219@gmail.com>
2025-01-17 14:45:33 -0600

    working upload

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const session = require("express-session");
 const cors = require("cors");
+const path = require("path");
+const multer = require("multer");
 const passport = require("passport");
 
 const connectDB = require("./config/database.js");
@@ -46,12 +48,39 @@ const corsOptions = {
     optionsSuccessStatus: 200, // For legacy browser support
 };*/
 
+var storage = multer.diskStorage({
+    //destination: function (req, file, cb) { keep hidden in production for now, don't want files constantly getting stored lol
+    //  console.log(file);
+
+    //cb(null, path.join(__dirname, "/uploads/"));
+    //},
+    filename: function (req, file, cb) {
+        cb(
+            null,
+            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+        );
+    },
+    fileFilter: function (req, file, callback) {
+        console.log(file.originalname);
+        var ext = path.extname(file.originalname);
+        if (ext !== ".3mf" && ext !== ".stl") {
+            return callback(new Error("Only models are allowed"));
+        }
+        callback(null, true);
+    },
+});
+const upload = multer({ storage: storage });
+const fileUpload = upload.fields([{ name: "file", maxCount: 1 }]);
+
+app.post("/upload", fileUpload, (req, res) => {
+    res.json({ message: "image successfully uploaded" });
+});
+
 app.use("/issues", require("./routes/issues.router.js"));
 app.use("/announcements", require("./routes/announcements.router.js"));
 app.use("/users", require("./routes/users.router.js"));
 app.use("/equipment", require("./routes/equipment.router.js"));
 app.use("/materials", require("./routes/materials.router.js"));
-
 
 app.get("/", (req, res) => {
     res.send("Ello :D");
