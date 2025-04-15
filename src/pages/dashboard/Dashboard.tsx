9d83b2ac898a50244724e5913746e22d4d12719e
draw-n <helen.wu1219@gmail.com>
2025-04-15 10:12:14 -0500

    past month chart for remote printing

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -16,7 +16,7 @@ const Dashboard: React.FC = () => {
                 <h1>DASHBOARD</h1>
 
                 <Row gutter={[16, 16]}>
-                    <Col span={12}>
+                    <Col span={24}>
                         <Card>
                             <Space
                                 style={{ width: "100%" }}
