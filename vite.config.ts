ab9f68b540a2fc4768d945a17da9467109e907eb
draw-n <helen.wu1219@gmail.com>
2025-09-02 17:58:26 -0500

    ugh

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
- base: "/",
- plugins: [react()],
- preview: {
-  port: 80,
-  strictPort: true,
- },
- server: {
-  port: 80,
-  strictPort: true,
-  host: true,
-  origin: "http://0.0.0.0:80",
- },
+    base: "/",
+    plugins: [react()],
+    server: {
+        port: 80,
+        strictPort: true,
+        host: true,
+    },
 });
