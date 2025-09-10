77ed1e22e4b516302e420fe1b20e7994de340b26
draw-n <helen.wu1219@gmail.com>
2025-09-10 15:05:35 -0500

    fixing modal

--- src/pages/remotePrint/Review.tsx
+++ src/pages/remotePrint/Review.tsx
@@ -171,16 +171,12 @@ const Review: React.FC<ReviewProps> = ({
                         confirmText="Is the bed clear? If not, please select cancel and try again at another time."
                         target={<Button type="primary">Submit</Button>}
                     >
-                        <iframe
-                            id="camera-iframe"
-                            src={import.meta.env.VITE_CAMERA_URL}
-                            style={{
-                                width: "100%",
-                                height: "400px",
-                                borderRadius: "10px",
-                                overflow: "hidden",
-                            }}
-                        />
+                        <div id="camera-iframe-container">
+                            <iframe
+                                id="camera-iframe"
+                                src={import.meta.env.VITE_CAMERA_URL}
+                            />
+                        </div>
                     </ConfirmAction>
                 </Flex>
             </Space>
