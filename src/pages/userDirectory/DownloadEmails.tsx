099828d1c9f7d5787bb1a582b03b959bc4ed5049
draw-n <helen.wu1219@gmail.com>
2025-09-10 15:43:12 -0500

    trying to add equipment pages back

--- src/pages/userDirectory/DownloadEmails.tsx
+++ src/pages/userDirectory/DownloadEmails.tsx
@@ -12,7 +12,7 @@ const DownloadEmails: React.FC = () => {
             const url = window.URL.createObjectURL(new Blob([response.data]));
             const link = document.createElement("a");
             link.href = url;
-            link.setAttribute("download", "users.csv");
+            link.setAttribute("download", "emails.txt");
             document.body.appendChild(link);
             link.click();
             link.remove();
